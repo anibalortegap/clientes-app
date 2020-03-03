4907eedf842ad44a185358188fc69ace803ec6bd
Anibal Ortega Piedrahita <anibalortegap@gmail.com>
2020-03-03 11:19:16 -0500

    add services client and metods

---
+++ src/app/clientes/clients.service.ts
@@ -0,0 +1,59 @@
+import { Client } from './client';
+import { Group } from './group';
+import { ClientesModule } from './clientes.module'
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: ClientesModule
+})
+export class ClientsService {
+
+  private clientes: Client[];
+  private grupos: Group[];
+
+  constructor() {
+
+    this.grupos = [
+      {
+        id: 0,
+        nombre: 'Sin definir'
+      },
+      {
+        id: 1,
+        nombre: 'Activos'
+      },
+      {
+        id: 2,
+        nombre: 'Inactivos'
+      },
+      {
+        id: 3,
+        nombre: 'Deudores'
+      }
+    ];
+    this.clientes = [];
+   }
+
+   getGrupos(){
+     return this.grupos;
+   }
+
+   getClient(){
+     return this.clientes;
+   }
+
+   addCliente(cliente: Client){
+     this.clientes.push(cliente);
+   }
+
+   newCLiente(): Client {
+     return {
+       id: this.clientes.length,
+       nombre: '',
+       cif: '',
+       direccion: '',
+       grupo: []
+     };
+   }
+}
